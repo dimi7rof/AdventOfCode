7c13cdf6f8e09b5e70ecd63f5aafe7b3fe3db1b2
dimi7rof <84029988+dimi7rof@users.noreply.github.com>
2025-10-25 19:10:25 +0300

    Update Program.cs for Advent of Code 2025

--- Console2025/Program.cs
+++ Console2025/Program.cs
@@ -1,15 +1,22 @@
-﻿using AdventOfCode.Solutions;
+using AdventOfCode.Solutions;
 
-Console.WriteLine("Enter a day (1-25) for Advent of Code 2025:");
-var day = int.Parse(Console.ReadLine() ?? "1");
+Console.WriteLine("Enter a day (1-25) for Advent of Code 2025 (press Enter for current day):");
+var input = Console.ReadLine();
+var day = string.IsNullOrEmpty(input) ? DateTime.Now.Day : int.Parse(input);
+
+if (day < 1 || day > 25)
+{
+    Console.WriteLine("Day must be between 1 and 25!");
+    return;
+}
 
 Console.WriteLine($"Enter input for Day {day} of Advent of Code 2025:");
-var input = Console.ReadLine() ?? "";
+var puzzleInput = Console.ReadLine() ?? "";
 
 var result = day switch
 {
-    1 => Solutions2025.Day1(input),
-    2 => Solutions2025.Day2(input),
+    1 => Solutions2025.Day1(puzzleInput),
+    2 => Solutions2025.Day2(puzzleInput),
     _ => (0, 0) // Default case for unsupported days
 };
 
